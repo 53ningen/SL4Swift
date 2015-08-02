2609d582e054e9af48e3e33bf70353eb0f5578e5
53ningen <korun24276@gmail.com>
2015-08-02 18:52:45 +0900

    enable to inject Writer into Logger

--- SL4Swift/LoggerFactory.swift
+++ SL4Swift/LoggerFactory.swift
@@ -7,5 +7,9 @@ public final class LoggerFactory {
     public static func getLogger(logLevel: LogLevel) -> Logger {
         return Logger(logLevel: logLevel)
     }
+    
+    public static func getLogger(logLevel: LogLevel, writer: (logLevel: LogLevel, string: String, file: String, line: Int, function: String) -> Void) -> Logger {
+        return Logger(logLevel: logLevel, writer: writer)
+    }
         
 }
